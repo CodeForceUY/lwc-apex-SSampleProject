ec826ce25f31e73abf7012cb1a70524513732c6d
Matias Repetto <repetto.matias3@gmail.com>
2022-08-03 17:42:24 -0300

    back and front fixes still WIP

--- takeanoteapp/force-app/main/default/lwc/noteItem/noteItem.js
+++ takeanoteapp/force-app/main/default/lwc/noteItem/noteItem.js
@@ -6,13 +6,7 @@ export default class NoteItem extends LightningElement {
     @api noteYear;
     @api descNote;
     @api mainNote;
-    @api saved;
-
-    noteAuthor = 'asd';
-    noteYear = 1234;
-    descNote = 'asda';
-    mainNote = 'asdas';
-    saved = false; 
+    @api saved; 
 
 
     get containerClass(){
